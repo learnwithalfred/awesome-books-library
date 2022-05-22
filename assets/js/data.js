cdabc16107ae862b2fe3ac7924f0a5fc46498862
Alfred Boateng <programmingwithalfred@gmail.com>
2022-05-22 21:18:17 +0000

    Add collection for books

---
+++ assets/js/data.js
@@ -0,0 +1,54 @@
+const data = [
+  {
+    id: 1,
+    title: "Santa's Apprentice",
+    author: 'Nicolle Plackstone',
+  },
+  {
+    id: 2,
+    title: 'History Is Made at Night',
+    author: 'Eduard Ekell',
+  },
+  {
+    id: 3,
+    title: 'Hercules',
+    author: 'Ofelia Lockless',
+  },
+  {
+    id: 4,
+    title: 'Shotgun Stories',
+    author: 'Miranda McCosh',
+  },
+  {
+    id: 5,
+    title: 'My Führer',
+    author: 'Barbara-anne Ancell',
+  },
+  {
+    id: 6,
+    title: 'Dear Diary (Caro Diario)',
+    author: 'Sanderson Duckhouse',
+  },
+  {
+    id: 7,
+    title: 'Little Trip to Heaven, A',
+    author: 'Wilma Worsall',
+  },
+  {
+    id: 8,
+    title: 'Innkeepers, The',
+    author: 'Crichton Junkison',
+  },
+  {
+    id: 9,
+    title: 'Maybe Baby',
+    author: 'Giles Lesser',
+  },
+  {
+    id: 10,
+    title: "Tough Guys Don't Dance",
+    author: 'Donia Brum',
+  },
+];
+
+export default data;
