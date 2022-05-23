903ba0df0b0ebd80c9eaeb2426efcd88110025c1
Alfred Boateng <programmingwithalfred@gmail.com>
2022-05-23 09:48:04 +0000

    Display all books saved in the collection in the top part of the page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,17 @@
 import data from './data.js';
 
-console.log(data);
+const bookList = document.querySelector('.book-list');
+
+const displayBooks = (arr) => {
+  for (let i = arr.length - 1; i >= 0; i -= 1) {
+    const fetch = document.querySelector('.book-list').innerHTML;
+
+    bookList.innerHTML = `<li class="book-item">
+          <span class="book">${data[i].title} by ${data[i].author}</span>
+          <button type="button" class="remove-book">Remove</button>
+        </li>
+              <hr />
+${fetch}`;
+  }
+};
+displayBooks(data);
