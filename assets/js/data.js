5540faaabfb702260dfac559c10d271eb5657c2c
Alfred Boateng <programmingwithalfred@gmail.com>
2022-05-23 16:59:36 +0000

    Update data to only 3

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,53 +1,18 @@
 const data = [
   {
-    id: 1,
+    id: '176543',
     title: "Santa's Apprentice",
     author: 'Nicolle Plackstone',
   },
   {
-    id: 2,
+    id: '223244556',
     title: 'History Is Made at Night',
     author: 'Eduard Ekell',
   },
   {
-    id: 3,
+    id: '3324',
     title: 'Hercules',
     author: 'Ofelia Lockless',
-  },
-  {
-    id: 4,
-    title: 'Shotgun Stories',
-    author: 'Miranda McCosh',
-  },
-  {
-    id: 5,
-    title: 'My Führer',
-    author: 'Barbara-anne Ancell',
-  },
-  {
-    id: 6,
-    title: 'Dear Diary (Caro Diario)',
-    author: 'Sanderson Duckhouse',
-  },
-  {
-    id: 7,
-    title: 'Little Trip to Heaven, A',
-    author: 'Wilma Worsall',
-  },
-  {
-    id: 8,
-    title: 'Innkeepers, The',
-    author: 'Crichton Junkison',
-  },
-  {
-    id: 9,
-    title: 'Maybe Baby',
-    author: 'Giles Lesser',
-  },
-  {
-    id: 10,
-    title: "Tough Guys Don't Dance",
-    author: 'Donia Brum',
   },
 ];
 
