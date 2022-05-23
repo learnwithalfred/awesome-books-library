812518b1d8324009e858e4ddbaa1e3bfba66e53f
Alfred Boateng <programmingwithalfred@gmail.com>
2022-05-23 17:00:00 +0000

    Update remove book function

--- assets/js/remove-book.js
+++ assets/js/remove-book.js
@@ -1,3 +1,10 @@
-const removeBook = (arr = [], id = '') => arr.filter((e) => e.id !== id);
+const removeBook = (arr = [], id = '') => {
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i].id === id) {
+      arr.splice(i, 1);
+    }
+  }
+  return arr;
+};
 
 export default removeBook;
